3e0eeb0122f43529655367ee3815d3984cfa1551
lolotrgeek <jon@simplyenable.com>
2025-01-16 17:14:06 -0600

    main: run continuously, display individuals and species in sidebar

--- main.ts
+++ main.ts
@@ -1,66 +1,87 @@
-import http from 'http';
-import { Evolution } from "./src/Evolution";
-import { GenePool } from "./src/GenePool";
-import { Brain } from './src/brain/Brain';
-import { TestEnvironment } from './src/testEnv';
+import { createServer, IncomingMessage, ServerResponse } from 'http'
+import { parse } from 'url';
+import { Evolution } from './src/Evolution'
+import { GenePool } from './src/GenePool'
+import { TestEnvironment } from './src/testEnv'
 
-const input_size = 3;
-const output_size = 2;
-const inputs = [1, 1, 1];
+const input_size = 3
+const output_size = 2
+const population_size = 1000
+const gene_pool = new GenePool()
+const evolution = new Evolution(input_size, output_size, population_size, gene_pool)
+const environment = new TestEnvironment(evolution, gene_pool)
 
-const population_size = 100
+evolution.reset(input_size, output_size, population_size, gene_pool)
+environment.populate(population_size)
+// console.table(environment.individuals().map(individual => ({ id: individual.id, species: individual.species })))
 
-const gene_pool = new GenePool();
-const evolution = new Evolution(input_size, output_size, population_size, gene_pool);
-const environment = new TestEnvironment(evolution, gene_pool);
+setInterval(() => environment.step(), 1000)
 
-evolution.reset(input_size, output_size, population_size, gene_pool);
-environment.populate(population_size);
+// console.table(environment.individuals().map(individual => ({ id: individual.id, weights: individual.genome.connections.getAll().map(c => c.weight) })))
 
-// take 100 steps
-for (let i = 0; i < 100; i++)environment.step();
-
-let genome = environment.individuals[0].genome;
-
-console.log(genome)
-
-const server = http.createServer((req, res) => {
+const server = createServer((req: IncomingMessage, res: ServerResponse) => {
   res.setHeader('Content-Type', 'application/json');
 
-  switch (req.url) {
+  if (!req.url) {
+    res.writeHead(400, { 'Content-Type': 'application/json' });
+    res.end(JSON.stringify({ message: 'Bad Request' }));
+    return
+  }
+
+  let urlParts = req.url ? req.url.split('?') : [];
+  let path = urlParts[0] ? urlParts[0] : req.url;
+  const speciesIndex = parseInt(parse(req.url, true).query.species as string);
+  const individualIndex = parseInt(parse(req.url, true).query.individual as string);
+
+
+  let genome;
+  const species = environment.species[speciesIndex];
+  if (species && species.individuals[individualIndex]) {
+    genome = species.individuals[individualIndex].genome;
+  }
+
+  switch (path) {
     case '/':
+      res.end(JSON.stringify(environment.species));
+      break;
+    case '/genome':
       res.end(JSON.stringify(genome));
       break;
     case '/mutateLink':
+      if (!genome) break;
       evolution.mutate_link(genome, gene_pool);
       res.end(JSON.stringify(genome));
       break;
     case '/mutateNode':
+      if (!genome) break;
       evolution.mutate_node(genome, gene_pool);
       res.end(JSON.stringify(genome));
       break;
     case '/mutateWeightShift':
+      if (!genome) break;
       evolution.mutate_weight_shift(genome);
       res.end(JSON.stringify(genome));
       break;
     case '/mutateWeightRandom':
+      if (!genome) break;
       evolution.mutate_weight_random(genome);
       res.end(JSON.stringify(genome));
       break;
     case '/mutateLinkToggle':
+      if (!genome) break;
       evolution.mutate_link_toggle(genome);
       res.end(JSON.stringify(genome));
       break;
     case '/stimulate':
-      // NTOE: we have to regenerate a new Brain every time the genome changes...
-      const brain = new Brain(genome);
-      const outputs = brain.think(inputs);
-      res.end(JSON.stringify(outputs));
-      break;
-    case '/reset':
-      evolution.reset(input_size, output_size, 100, gene_pool);
-      genome = evolution.new_genome(gene_pool)
-      res.end(JSON.stringify(genome));
+      if (!species || !species.individuals[individualIndex]) {
+        res.writeHead(400, { 'Content-Type': 'application/json' });
+        res.end(JSON.stringify({ message: 'Invalid species or individual index' }));
+        break;
+      }
+      const individual = species.individuals[individualIndex];
+      const randomInput = Array.from({ length: input_size }, () => Math.random());
+      const output = individual.brain.think(randomInput);
+      res.end(JSON.stringify(output));
       break;
     default:
       res.end(JSON.stringify({ message: 'Unknown endpoint' }));
@@ -69,5 +90,7 @@ const server = http.createServer((req, res) => {
 });
 
 server.listen(3000, () => {
-  console.log('Server listening on port 3000');
+  console.log('Server running on port 3000');
 });
+
+
