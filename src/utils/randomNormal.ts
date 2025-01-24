17266a16a9d5286a4a70d98018b8bf507e8c16d4
lolotrgeek <jon@simplyenable.com>
2025-01-24 10:34:27 -0600

    randomNormal: initial util

---
+++ src/utils/randomNormal.ts
@@ -0,0 +1,6 @@
+export function randomNormal(mean: number, stdDev: number): number {
+    let u1 = Math.random();
+    let u2 = Math.random();
+    let z0 = Math.sqrt(-2.0 * Math.log(u1)) * Math.cos(2.0 * Math.PI * u2);
+    return z0 * stdDev + mean;
+}
